9f8f1175e7baf0fc877c6d8797d473b8f9b26577
PavSamsungSsdWork <fire_ra@bk.ru>
2023-05-18 13:40:22 +0300

    some arrays

---
+++ app/src/main/java/com/example/android/codelabs/paging/experimental/SomeArrays.java
@@ -0,0 +1,12 @@
+package com.example.android.codelabs.paging.experimental;
+
+import java.util.ArrayList;
+
+public class SomeArrays {
+
+    ArrayList<String> re = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
+}
