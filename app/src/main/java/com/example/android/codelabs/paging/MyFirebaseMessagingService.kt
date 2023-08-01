f1dc974bd5ecdd9b01864027f38c5591e682d6aa
Pavel Dolganov mSt <fire_ra@bk.ru>
2023-08-01 13:53:37 +0300

    FCM start

---
+++ app/src/main/java/com/example/android/codelabs/paging/MyFirebaseMessagingService.kt
@@ -0,0 +1,11 @@
+package com.example.android.codelabs.paging
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyFirebaseMessagingService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
